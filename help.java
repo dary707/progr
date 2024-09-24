593315e12b80734d51108bd1d021781d89886790
halmatova <darya.halmatova@mail.ru>
2024-09-24 15:11:24 +0700

    Добавлено описание циклов, изменено поведение программы Добавили обработку пользователя, в случае ошибочного выбора пользователя, в случае ошибочного выбора показывается заново содержание меню

--- help.java
+++ help.java
@@ -1,12 +1,18 @@
-class Help {
+class help {
         public static void main(String args[]) throws java.io.IOException {
-                char choice;
-                System.out.println("Справка по операторам: ");
-                System.out.println(" 1. if ");
-                System.out.println(" 2. switch ");
-                System.out.println("Выберите номер раздела: ");
-                choice = (char) System.in.read();
+                char choice,ignore;
+		do {
+                	System.out.println("Справка по операторам: ");
+                	System.out.println(" 1. if ");
+                	System.out.println(" 2. switch ");
+                	System.out.println("Выберите номер раздела: ");
+                	choice = (char) System.in.read();
+			do {
+				ignore = (char) System.in.read();
+			} while (ignore != '\n');
+		} while (choice <'1' | choice > 'S');
 
+		
                 System.out.println("\n");
                 switch(choice) {
                         case '1':
@@ -23,6 +29,21 @@ class Help {
                                 System.out.println(" ... ");
                                 System.out.println("} ");
                                 break;
+			case '3':
+				System.out.println("Цикл for:\n");
+				System.out.println("for (инициализация; условие; итерция");
+				System.out.println(" оператор;");
+				break;
+			case '4':
+				System.out.println("Цикл while:\n");
+				System.out.println("while (условие) оператор;");
+				break;
+			case '5':
+				System.out.println("Цикл do-while:\n");
+				System.out.println("do {'");
+				System.out.println(" оператор;");
+				System.out.println("while (условие);");
+				break;
                         default:
                                 System.out.println("Неверный выбор раздела");
                 }
