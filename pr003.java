8118c219ae96830c19b75acbabf06169f05a522e
halmatova <darya.halmatova@mail.ru>
2024-09-24 13:06:44 +0700

    Варианты использования цикла for

--- pr003.java
+++ pr003.java
@@ -71,9 +71,34 @@ class pr003{
 						System.out.println("Внутренний switch");
 						break;
 				}
+			       break;
 			case 'h':
                                System.out.println("Не выведется");
-                               break;
-		}  
+			       break;
+	  }		       
+          //Оператор цикла for
+	  //
+	  System.out.println("\n");
+	  double num, sroot, rerr;
+
+	  for(num=1.0; num < 100.0; num++) {
+		  sroot = Math.sqrt(num);
+		  System.out.println("Квадратный корень числа " + num +" равен" + sroot);
+		  //Вычисление ошибки округления 
+		  rerr = num - (sroot * sroot);
+		  System.out.println("Ошибка округления оставляет "+ rerr);
+		  System.out.println();
+	  }
+
+	  int x1;
+	  for(x1 = 100; x1 > -100; x1 -= 5)
+		  System.out.println(x1);
+	  int i7, j7;
+	  for (i7 = 0, j7 = 10; i7 < j7; i7++, j7--)
+		  System.out.println("i7 и j7: " + i7 + " " +j7);
+	  int i8;
+	  System.out.println("Для остановки цикла нажмите клавишу \"S\"");
+	  for (i8 = 0; (char) System.in.read() != 'S'; i8++)
+		  System.out.println("Итерация №" + i8);
 	}
 }       
