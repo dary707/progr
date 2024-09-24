08ff54f896b087a1282117b55afa9330a805455a
halmatova <darya.halmatova@mail.ru>
2024-09-24 16:18:20 +0700

    Добавлено использование оператора break

--- pr003.java
+++ pr003.java
@@ -75,74 +75,120 @@ class pr003{
 			case 'h':
                                System.out.println("Не выведется");
 			       break;
-	  }		       
-          //Оператор цикла for
-	  //
-	  System.out.println("\n");
-	  double num, sroot, rerr;
+	 	 }		       
+         	 //Оператор цикла for
+	 	 System.out.println("\n");
+		 double num, sroot, rerr;
+		 
+		 for(num=1.0; num < 100.0; num++) {
+			sroot = Math.sqrt(num);
+		  	System.out.println("Квадратный корень числа " + num +" равен" + sroot);
+		  	//Вычисление ошибки округления 
+		  	rerr = num - (sroot * sroot);
+		  	System.out.println("Ошибка округления оставляет "+ rerr);
+		  	System.out.println();
+	  	}
 
-	  for(num=1.0; num < 100.0; num++) {
-		  sroot = Math.sqrt(num);
-		  System.out.println("Квадратный корень числа " + num +" равен" + sroot);
-		  //Вычисление ошибки округления 
-		  rerr = num - (sroot * sroot);
-		  System.out.println("Ошибка округления оставляет "+ rerr);
-		  System.out.println();
-	  }
+	  	int x1;
+	  	for(x1 = 100; x1 > -100; x1 -= 5)
+		  	System.out.println(x1);
 
-	  int x1;
-	  for(x1 = 100; x1 > -100; x1 -= 5)
-		  System.out.println(x1);
-	  int i7, j7;
-	  for (i7 = 0, j7 = 10; i7 < j7; i7++, j7--)
-		  System.out.println("i7 и j7: " + i7 + " " +j7);
-	  int i8;
-	  System.out.println("Для остановки цикла нажмите клавишу \"S\"");
-	  for (i8 = 0; (char) System.in.read() != 'S'; i8++)
-		  System.out.println("Итерация №" + i8);
-	  System.out.println("\n");
-
-	  for (i8 = 0; i8 < 10;) {
-		  System.out.println("Итерация №" + i8);
-		  i8++;
-	  }
-          System.out.println("\n");
-  	  for (i8 = 0; i8 < 20;) {
+		System.out.println("\n");
+	  	int i7, j7;
+	  	for (i7 = 0, j7 = 10; i7 < j7; i7++, j7--)
+			  System.out.println("i7 и j7: " + i7 + " " +j7);
+	  	int i8;
+		char ignore;
+	  	System.out.println("Для остановки цикла нажмите клавишу \"S\"");
+	  	for (i8 = 0; (char) System.in.read() != 'S'; i8++)
+		  	do {
+			  	ignore = (char) System.in.read();
+		  	} while (ignore != '\n');
+		  	System.out.println("Итерация №" + i8);
+       
+	  	System.out.println('\n');
+	  	
+		for (i8=0; i8 < 10;) {
+		       	System.out.println("Итерация №" + i8);
+		  	i8++;
+	  	}	
+          	
+		System.out.println("\n");
+	  	for (i8 = 0; i8 < 20;) {
                   System.out.println("Итерация №" + i8);
                   i8++;
-	  }
+	  	}
 
-	  //Бесконечный цикл:
-	  //System.out.println("\n");
-	  //for (;;);
-	  System.out.println("\n");
+		System.out.println("\n");
+	  	//Бесконечный цикл:
+	  	for (;;) {
+		  	ch = (char) System.in.read();
+		  	if (ch=='q') break;
+		}
 
-	  int sum = 0;
-	  //Вычисление сумм чисел от 1 до 5
-	  for (i8 = 1; i8 <= 5; sum += i8++);
-	  System.out.println("Сумма чисел равна "+ sum);
-	  //Цикл While 
-	  System.out.println("\n");
-	  ch = 'а';
-	  while (ch <= 'я') {
-		  System.out.print(ch);
-		  ch++;
-          }
-	  System.out.println();
+	  	System.out.println("\n");
 
-	  System.out.println("\n");
-	  // Вычисление целых степеней числа 2 
+	  	int sum = 0;
 
-	  int e;
-	  int result;
-	  for (int i9=0; i9 <10; i9++) {
-		  result = 1;
-		  e = i9;
-		  while(e > 0) {
-			  result *=2;
-			  e--;
-		  }
-		  System.out.println("2 в степени " + i9 + " равно " + result);
-	  }
+	  	//Вычисление сумм чисел от 1 до 5 
+	  	for (i8 = 1; i8 <= 5; sum += i8++);
+	  	System.out.println("Сумма чисел равна "+ sum);
+	  	//Цикл While 
+	  	System.out.println("\n");
+	  	ch = 'а';
+	  	while (ch <= 'я') {
+		  	System.out.println(ch);
+		  	ch++;
+          	}	
+	  	System.out.println();
+
+	  	System.out.println("\n");
+	  	//Вычисление целых степеней числа 2 
+
+	  	int e;
+	  	int result;
+	  	for (int i9=0; i9 <10; i9++) {
+		  	result = 1;
+		  	e = i9;
+		  	while(e > 0) {
+			  	result *=2;
+			  	e--;
+		  	}
+	 	  	System.out.println("2 в степени " + i9 + " равно " + result);
+	 }
+	
+		 	//Цикл do-while 
+		 	System.out.println("/n");
+	 	 	do {
+		 		System.out.print("Нажмите клавишу, затем ENTER: ");
+		 		ch = (char) System.in.read();
+		 		do {
+			 		ignore = (char) System.in.read();
+		 		} while (ignore != '\n');
+	 	 	}while (ch !='q');
+	 	
+			//Оператор break 
+			System.out.println("\n");
+		
+			for (x1 = 100; x1 > -100; x1 -= 5) {
+		      		System.out.println(x1);
+	      	      		if (x1 == 50) 
+					break; // прекращаем выполнение цикла
+			}
+  		
+			System.out.println("\n");
+			//break с вложенными циклами
+			for (int i9=0; i9<3; i9++) {
+				System.out.println("Счетчик внешнего цикла: "+ i9);
+				System.out.print("Счетчик внешнего цикла: ");
+				int t = 0;
+				while (t < 100) {
+					if(t==10) break;
+					System.out.print(t + " ");
+					t++;
+				}
+				System.out.println();
+			}
+			System.out.println("Циклы закончились");
 	}
 }       
