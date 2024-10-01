70f2301652dba1ed7a07477d22ab163d08081d39
halmatova <darya.halmatova@mail.ru>
2024-10-01 13:19:51 +0700

    Добавлен оператор break с пометкой

--- pr003.java
+++ pr003.java
@@ -190,5 +190,67 @@ class pr003{
 				System.out.println();
 			}
 			System.out.println("Циклы закончились");
-	}
-}       
+			
+			System.out.println("\n");
+
+			//Исспользование оператора break с меткой 
+			for (int i9=1; i9 <4; i9++) {
+one:				{
+two:					{			
+three:						{
+							System.out.println("\ni9 равно: " + i9);
+							if(i9==1) break one;
+							if(i9==2) break two;
+							if(i9==3) break three;
+							//Не выводится строка:
+							System.out.println("Окончание цикла");
+						}
+						System.out.println("После блока tree");
+					}
+					System.out.println("После блока two");
+				}
+				System.out.println("После блока one");
+			}
+			System.out.println("После цикла for с метками");
+
+			//Второй пример использования оператора break  с пометкой 
+			{
+done: 				
+				for(int i9=0; i9<10; i9++) {
+					for(int j9=0; j9<10; j9++) {
+						for (int k9=0; k9<10; k9++) {
+							System.out.println(k9 + " ");
+							if(k9 == 5) break done; //переход по метке
+						}
+						System.out.println("После цикла k9");//не выполнится
+					}
+					System.out.println("После цикла i9"); //не выполнится
+				}
+				System.out.println("После цикла i9");
+			}
+			
+				
+				
+			// Третий пример break с меткой
+			System.out.println("\n");
+			// Метка перед оператором for
+stop1:			for (int i9=0;i9<5;i9++) {
+				for(int j9=0; j9<5; j9++) {
+					if(j9 == 2) break stop1;
+					System.out.println("i9 и j9: " + i9 + " " + j9 );
+
+				}
+			}
+				System.out.println("\n");
+			for(int i9=0;i9<5;i9++) 
+stop2:			{
+				for(int j9=0; j9<5; j9++) {
+					if(j9 == 2) break stop2;
+					System.out.println("i9 и j9: " + i9 + " ");
+				}
+			}
+			//Использование оператора continue (прерывание текущей итерации)
+	
+       }
+}		
+
