261a8ed22cd189f8141422cd2a594f2d1bd60b43
dary707 <darya.halmatova@mail.ru>
2024-11-15 00:07:39 +0700

    Добавили пример рекурсии и демонстрацию использования статических переменных

--- pr008.java
+++ pr008.java
@@ -108,3 +108,108 @@ class VarArgs {
     System.out.println(); 
   } 
 } 
+class pr008 { 
+  public static void main(String[] args) { 
+ 
+    Factorial f = new Factorial(); 
+ 
+    System.out.println("Вычисление факториала рекурсивным методом"); 
+    System.out.println("Факториал 3 равен " + f.factR(3)); 
+    System.out.println("Факториал 4 равен " + f.factR(4)); 
+    System.out.println("Факториал 5 равен " + f.factR(5)); 
+ 
+    System.out.println(); 
+     
+    System.out.println("Вычисление факториала итеративным методом"); 
+    System.out.println("Факториал 3 равен " + f.factI(3)); 
+    System.out.println("Факториал 4 равен " + f.factI(4)); 
+    System.out.println("Факториал 5 равен " + f.factI(5)); 
+ 
+    //Демонстрация использования статической переменной 
+    StaticDemo ob1 = new StaticDemo(); 
+    StaticDemo ob2 = new StaticDemo(); 
+ 
+    ob1.x = 10; 
+    ob2.x = 20; 
+ 
+    System.out.println("ob1.x и ob2.x независимы друг от друга"); 
+    System.out.println("ob1.x: " + ob1.x + "\nob2.x" + ob2.x); 
+ 
+    System.out.println(); 
+     
+    System.out.println("Статическая переменная y является общей"); 
+    StaticDemo.y = 19; 
+    System.out.println("Для y = 19"); 
+ 
+    System.out.println("ob1.sum(): " + ob1.sum()); 
+    System.out.println("ob2.sum(): " + ob2.sum()); 
+ 
+    StaticDemo.y = 100; 
+    System.out.println("Для y = 100"); 
+ 
+    System.out.println("ob1.sum(): " + ob1.sum()); 
+    System.out.println("ob2.sum(): " + ob2.sum()); 
+ 
+    //Демострация работы статического блока 
+    System.out.println(); 
+    StaticBlock ob = new StaticBlock("Внутри конструктора"); 
+ 
+    System.out.println("Квадратный корень из 2: " + StaticBlock.rootof2); 
+    System.out.println("Квадратный корень из 3: " + StaticBlock.rootof3); 
+ 
+    System.out.println(); 
+    //Демонстрация работы внутреннего класса 
+    int[] x = {3, 2, 1, 5, , 9, 7, 2}; 
+    Outer outOb = new Outer(x); 
+ 
+    outOb.analyze(); 
+ 
+    class ShowBt { 
+      int numbits; 
+      ShowBt(int n) { 
+        numbits = n; 
+        } 
+        void show(long val) { 
+          long mask = 1; 
+    //Сдвиг влево значения 1 
+          mask <<= numbits-1; 
+ 
+          int spacer = 0; 
+          for (;mask!=0;mask>>=1) { 
+            if ((val & mask) != 0) 
+              System.out.print("1"); 
+            else System.out.print("0"); 
+              spacer++; 
+              if((spacer%8) == 0) { 
+                System.out.print(" "); 
+                spacer = 0; 
+              } 
+          } 
+        System.out.println(); 
+        } 
+    } 
+    System.out.println() 
+    for(byte b=0; b<10; b++) { 
+      ShowBt byteval = new ShowBt(8); 
+      System.out.print(b + " в двоичном виде: "); 
+      byteval.show(b); 
+    } 
+ 
+    System.out.println(); 
+    //Демонстрация вызова метода с переменным количеством аргументов 
+    VarArgs.vaTest(10); 
+    VarArgs.vaTest(1, 2, 3); 
+    VarArgs.vaTest(); 
+ 
+    System.out.println(); 
+    VarArgs.vaTest2("Один аргумент переменной длины: ", 10); 
+    VarArgs.vaTest2("Два аргумента переменной длины: ", 1, 2, 3); 
+    VarArgs.vaTest2("Без аргументов переменной длины: "); 
+ 
+    System.out.println(); 
+    VarArgs.vaTest2(10, 20); 
+    VarArgs.vaTest2("Два аргумента переменной длины: ", 1, 2, 3); 
+    VarArgs.vaTest2(true, false, false); 
+  } 
+}
+
