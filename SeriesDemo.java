3fc66b0e15a2d357c67bb058511edcbb6d5a6a82
halmatova <darya.halmatova@mail.ru>
2024-12-10 13:27:40 +0700

    Добавили класс ByThrees, демонстрация интерфейсной переменной

--- SeriesDemo.java
+++ SeriesDemo.java
@@ -14,5 +14,34 @@ class SeriesDemo {
 		for(int i=0; i < 5; i++)
 			System.out.println("Следующее значение в последовательности: " + ob.getNext());
 		System.out.println("Последнее предыдущее значение: " + ob.getPrevious());
+		
+		//Последовательность "через 3"
+		System.out.println();
+		System.out.println("\nНовая последовательность");
+		for(int i=0; i < 5; i++)
+			System.out.println("Следующее значение в последовательности: " + ob3.getNext());
+		//System.out.println("Последнее предыдущее значение: " + ob.getPrevious());
+
+		System.out.println("\nСброс последовательности к статровому значению");
+		ob3.reset();
+		for(int i = 0; i< 5; i++)
+			System.out.println("Следующее значение в последовательности: " + ob3.getNext());
+		//System.out.println("Последнее предыдущее значение: " + ob.getPrevious());
+		
+		System.out.println("\nУстанавливаем новое стартовое значене" );
+		ob3.setStart(100);
+		for(int i = 0; i < 5; i++)
+			System.out.println("Следующее значение в последовательности: " + ob3.getNext());
+		//System.out.println("Последнее предыдущее значение: " + ob.getPrevious());
+		//
+		//Демонстрация использования интерфейсной переменной
+		Series obInt;
+
+		for(int i = 0; i <5; i++) {
+			obInt = ob;
+			System.out.println("Следующее значение в последовательности ByTows: "+ obInt.getNext());
+			obInt = ob3;
+			System.out.println("Следующее значение в последовательности ByTows: "+ obInt.getNext());
+		}	
 	}
 }
