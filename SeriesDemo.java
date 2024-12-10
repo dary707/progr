7382288a7f2766b840b1ce521706010c625877c4
halmatova <darya.halmatova@mail.ru>
2024-12-10 12:56:53 +0700

    Добавили ByTows метод получения предыдущего значения

---
+++ SeriesDemo.java
@@ -0,0 +1,18 @@
+class SeriesDemo {
+	public static void main(String[] args) {
+		ByTwos ob = new ByTwos();
+		for(int i = 0; i < 5; i++)
+			System.out.println("Следующее значение в последовательности: " + ob.getNext());
+		System.out.println("Последнее предыдущее значение: " + ob.getPrevious());
+		System.out.println("\nСброс последовательности к стартовому значению");
+		ob.reset();
+		for(int i=0; i < 5; i++)
+			System.out.println("Следующее значение в последовательности: " + ob.getNext());
+		System.out.println("Последнее предыдущее значение: " + ob.getPrevious());
+		System.out.println("\nУстанавливаем стартовое значение");
+		ob.setStart(100);
+		for(int i=0; i < 5; i++)
+			System.out.println("Следующее значение в последовательности: " + ob.getNext());
+		System.out.println("Последнее предыдущее значение: " + ob.getPrevious());
+	}
+}
